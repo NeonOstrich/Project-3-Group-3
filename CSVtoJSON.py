79a38fb9ded6af532f71ccc5dee27e0cea3ebd2b
MuffinMuppet <you@example.com>
2023-04-17 18:59:38 -0400

    JSON

--- CSVtoJSON.py
+++ CSVtoJSON.py
@@ -1,14 +1,14 @@
 import csv
 import json
 
-# Open CSV file for reading
-with open('Project 3\static\js\InfantMortalityRate.csv', 'r') as csv_file:
-    # Create a CSV reader
-    csv_reader = csv.DictReader(csv_file)
-    
+import csv
+import json
+
+# Open CSV file for reading with specified encoding format
+with open('Project 3/static/js/InfantMortalityRate.csv', 'r', encoding='latin-1') as file:
+    csv_reader = csv.reader(file)
     # Create an empty list to store the JSON objects
     json_list = []
-    
     # Iterate through each row in the CSV file
     for row in csv_reader:
         # Append each row as a dictionary to the JSON list
@@ -18,3 +18,4 @@ with open('Project 3\static\js\InfantMortalityRate.csv', 'r') as csv_file:
 with open('output.json', 'w') as json_file:
     # Use the json.dump() function to write the JSON list to the file
     json.dump(json_list, json_file, indent=4)
+
