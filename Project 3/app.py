6fee11b15b28e6970c9ce102db72477b7887bb45
NeonOstrich <oliver.zagorin@gmail.com>
2023-04-17 21:57:26 -0400

    4 17

--- Project 3/app.py
+++ Project 3/app.py
@@ -1,22 +1,24 @@
 import pandas as pd
-from flask import Flask, jsonify
+import numpy as np
+from sqlalchemy import create_engine
+from flask import Flask, jsonify, render_template
 
-file = "Project 3\static\js\InfantMortalityRate.csv"
-table = pd.read_csv(file, encoding='latin-1')
-json_format = table.to_json(orient="index")
+engine = create_engine("sqlite:///infant_mortality.db")
 
 app = Flask(__name__)
 
 @app.route("/")
-def data():
-    return json_format
+def index():
+    return render_template('index.html')
+
+
+
+@app.route("/api/data")
+def data_func():
+    conn = engine.connect()
+    df = pd.read_sql('Select * From infant_mortality', conn)
+    data = df.to_dict(orient="records")
+    return jsonify(data)  # Use Flask's jsonify function to return JSON data
 
 if __name__ == '__main__':
     app.run(debug=True)
-
-
-
-@app.route("/")
-def data():
-    return jsonify(json_format)  # Use Flask's jsonify function to return JSON data
-
