e5d14a04201186553857ec5d3c2307ddde4e9852
MuffinMuppet <you@example.com>
2023-04-19 18:24:09 -0400

    NEW

--- Project 3/static/js/example.js
+++ Project 3/static/js/example.js
@@ -1,1 +1,9 @@
 console.log("Hello from javascript")
+
+// Read in data from Flask
+const url = "http://127.0.0.1:5000/api/data"
+
+d3.json(url).then(function(data) {
+console.log(data)
+// Close the .then function
+});
