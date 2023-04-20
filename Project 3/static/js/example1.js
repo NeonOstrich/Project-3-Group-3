829eaaaef960024bfd6ec94d7fe8a80f62769dfd
NeonOstrich <oliver.zagorin@gmail.com>
2023-04-19 20:18:42 -0400

    updated html and example 1

--- Project 3/static/js/example1.js
+++ Project 3/static/js/example1.js
@@ -10,9 +10,21 @@
 // function loadData(year) {
 // 	$.getJSON('/api
 
-const data_url = "http://127.0.0.1:3008/api/data"
+const data_url = "http://127.0.0.1:5000/api/data"
 // console.log(data2)
 d3.json(data_url).then((data) => {
+    gender = data.map((item)=> item["Gender"])
+    imr = data.map((item)=> item["Infant Mortality Rate"])
+    console.log(country)
+    var data2 = [
+        {
+          x: gender,
+          y: imr,
+          type: 'bar'
+        }
+      ];
+      
+
+      Plotly.newPlot('bar', data2);
 console.log(data)
 })
-
